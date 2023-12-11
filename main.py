b6e593c176cdb2d38afc1accd594105e75f2f04e
Pinto Tetteh Aaron <aaronpinto111@gmail.com>
2023-12-11 13:28:53 +0000

    update for deployment

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from models.shop_models import *
 from core.db import Base, engine
 from api.v1 import products, collections
@@ -7,6 +8,16 @@ Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(products.router)
 app.include_router(collections.router)
